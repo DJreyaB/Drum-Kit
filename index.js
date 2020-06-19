8a5c49a2ffe1d0a64717dead0965cdb41f89e5d5
djreyab <djreya.boyd@gmail.com>
2020-06-19 13:12:54 -0400

    switch case added for button and inner html correlation

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 let btnCount = document.querySelectorAll('.drum');
-let audio    = new Audio('./sounds/tom-1.mp3');
 
 for (let i = 0;i  < btnCount.length;i++){
     btnCount[i].addEventListener('click', function () {
-        audio.play();
+        
+        let btnHtml = this.innerHTML;
+
+        switch (btnHtml) {
+            case 'w':
+                let snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            case 'a':
+                let crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+            case 's':
+                let tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            case 'd':
+                let tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case 'j':
+                let kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+            case 'k':
+                let tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            case 'l':
+                let tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;           
+            default:  alert('there is a problem');
+        }
     });
 }
